2a5cc0d682967f5012827604714af89f00a657f2
Jairo Suarez <jasuarezpr@gmail.com>
2014-06-24 12:58:41 -0500

    commit inicial

---
+++ businessLogic/src/main/java/org/betfutbol/BusinessLogicBean.java
@@ -0,0 +1,16 @@
+package org.betfutbol;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+@Stateless
+@LocalBean
+public class BusinessLogicBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
